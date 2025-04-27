f665ce7f10fd71927939e6f45dc1d381406b474d
Adelabu Mobolaji <142588160+Bolazcoding@users.noreply.github.com>
2025-04-27 17:47:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-"use strict";
+'use strict';
 
-const menu_sidebar = document.querySelector(".aside-sidebar");
-const hamburger_open_menu = document.querySelector(".hamburger-menu-open");
-const hamburger_close_menu = document.querySelector(".hamburger-menu-close");
-const cart = document.querySelector(".cart");
-const open_cart = document.querySelector(".cart-checkout-container");
-const cartNumber = document.querySelector(".many-to-buy-text");
-const numberOfProduct = document.querySelector(".cart-num");
-const totalPriceProduct = document.querySelector(".total-price");
-const add_to_cart = document.querySelector(".add-to-cart");
-const icon_minus = document.querySelector(".icon-minus");
-const icon_plus = document.querySelector(".icon-plus");
-const cart_empty = document.querySelector(".cart-empty-body");
-const cart_checkout = document.querySelector(".cart-checkout-body");
-const zoomProduct = document.querySelector(".box-bigger");
-const overlay_slider = document.querySelector(".overlay");
-const close_slider = document.querySelector(".slider-close");
+const menu_sidebar = document.querySelector('.aside-sidebar');
+const hamburger_open_menu = document.querySelector('.hamburger-menu-open');
+const hamburger_close_menu = document.querySelector('.hamburger-menu-close');
+const cart = document.querySelector('.cart');
+const open_cart = document.querySelector('.cart-checkout-container');
+const cartNumber = document.querySelector('.many-to-buy-text');
+const numberOfProduct = document.querySelector('.cart-num');
+const totalPriceProduct = document.querySelector('.total-price');
+const add_to_cart = document.querySelector('.add-to-cart');
+const icon_minus = document.querySelector('.icon-minus');
+const icon_plus = document.querySelector('.icon-plus');
+const cart_empty = document.querySelector('.cart-empty-body');
+const cart_checkout = document.querySelector('.cart-checkout-body');
+const zoomProduct = document.querySelector('.box-bigger');
+const overlay_slider = document.querySelector('.overlay');
+const close_slider = document.querySelector('.slider-close');
+const imgChanged = document.querySelector('.image-box');
+const imgOverlayChanged = document.querySelector('.image-box-overlay');
+const btnLeft = document.querySelectorAll('.slider__btn--left');
+const btnRight = document.querySelectorAll('.slider__btn--right');
 
-const slides = document.querySelectorAll(".box");
-const btnLeft = document.querySelector(".slider__btn--left");
-const btnRight = document.querySelector(".slider__btn--right");
-
-zoomProduct.addEventListener("click", function () {
-  overlay_slider.classList.remove("hidden");
+zoomProduct.addEventListener('click', function () {
+  overlay_slider.classList.remove('hidden');
 });
 
-close_slider.addEventListener("click", function () {
-  overlay_slider.classList.add("hidden");
+close_slider.addEventListener('click', function () {
+  overlay_slider.classList.add('hidden');
 });
 
-hamburger_open_menu.addEventListener("click", function () {
-  menu_sidebar.style.display = "block";
+hamburger_open_menu.addEventListener('click', function () {
+  menu_sidebar.style.display = 'block';
 });
 
-hamburger_close_menu.addEventListener("click", function () {
-  menu_sidebar.style.display = "none";
+hamburger_close_menu.addEventListener('click', function () {
+  menu_sidebar.style.display = 'none';
 });
 
-cart.addEventListener("click", function () {
-  open_cart.classList.toggle("hidden-cart");
+cart.addEventListener('click', function () {
+  open_cart.classList.toggle('hidden-cart');
 });
 
 let cartNum = 0;
 
-icon_plus.addEventListener("click", function () {
+icon_plus.addEventListener('click', function () {
   cartNum++;
   cartNumber.textContent = cartNum;
   // console.log(cartNum);
 });
 
-icon_minus.addEventListener("click", function () {
+icon_minus.addEventListener('click', function () {
   if (cartNum > 0) {
     cartNum--;
     cartNumber.textContent = cartNum;
@@ -60,17 +60,106 @@ icon_minus.addEventListener("click", function () {
   }
 });
 
-add_to_cart.addEventListener("click", function () {
+add_to_cart.addEventListener('click', function () {
   if (cartNum === 0) {
-    cart_checkout.style.display = "none";
-    cart_empty.style.display = "block";
+    cart_checkout.style.display = 'none';
+    cart_empty.style.display = 'block';
     return;
   } else if (cartNum > 0) {
-    cart_empty.style.display = "none";
-    cart_checkout.style.display = "block";
+    cart_empty.style.display = 'none';
+    cart_checkout.style.display = 'block';
     numberOfProduct.textContent = cartNum;
-    totalPriceProduct.textContent = `${"$" + 125 * cartNum}`;
+    totalPriceProduct.textContent = `${'$' + 125 * cartNum}`;
   }
+});
+
+let images = [
+  './images/image-product-1.jpg',
+  './images/image-product-2.jpg',
+  './images/image-product-3.jpg',
+  './images/image-product-4.jpg',
+];
+
+let currentIndex = 0;
+
+btnRight.forEach(function (right) {
+  right.addEventListener('click', function () {
+    currentIndex = currentIndex + 1;
+
+    if (currentIndex == images.length) {
+      currentIndex = 0;
+    }
+
+    imgChanged.src = images[currentIndex];
+    imgOverlayChanged.src = images[currentIndex];
+    console.log(currentIndex);
+  });
+});
+
+btnLeft.forEach(function (left) {
+  left.addEventListener('click', function () {
+    currentIndex = currentIndex - 1;
+
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
+    }
+
+    imgChanged.src = images[currentIndex];
+    imgOverlayChanged.src = images[currentIndex];
+  });
+});
+
+const image1 = document.querySelectorAll('.image-thumbnail-1');
+const image2 = document.querySelectorAll('.image-thumbnail-2');
+const image3 = document.querySelectorAll('.image-thumbnail-3');
+const image4 = document.querySelectorAll('.image-thumbnail-4');
+
+image1.forEach(function (img) {
+  img.addEventListener('click', function () {
+    imgChanged.src = images[0];
+    imgOverlayChanged.src = images[0];
+
+    document.querySelectorAll('.image-thumbnail').forEach(function (e) {
+      e.classList.remove('active-img');
+    });
+    this.classList.add('active-img');
+  });
+});
+
+image2.forEach(function (img) {
+  img.addEventListener('click', function () {
+    imgChanged.src = images[1];
+    imgOverlayChanged.src = images[1];
+
+    document.querySelectorAll('.image-thumbnail').forEach(function (e) {
+      e.classList.remove('active-img');
+    });
+    this.classList.add('active-img');
+  });
+});
+
+image3.forEach(function (img) {
+  img.addEventListener('click', function () {
+    imgChanged.src = images[2];
+    imgOverlayChanged.src = images[2];
+
+    document.querySelectorAll('.image-thumbnail').forEach(function (e) {
+      e.classList.remove('active-img');
+    });
+    this.classList.add('active-img');
+  });
+});
+
+image4.forEach(function (img) {
+  img.addEventListener('click', function () {
+    imgChanged.src = images[3];
+    imgOverlayChanged.src = images[3];
+
+    document.querySelectorAll('.image-thumbnail').forEach(function (e) {
+      e.classList.remove('active-img');
+    });
+    this.classList.add('active-img');
+  });
 });
 
 // const slider = function () {
@@ -84,6 +173,7 @@ add_to_cart.addEventListener("click", function () {
 //     );
 //   };
 //   gotoSlide(0);
+
 //   // Next slide
 
 //   const nextSlide = function () {
